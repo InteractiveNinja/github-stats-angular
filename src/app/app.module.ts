c4d0535237a617c33b206a759c22d5cb6367fb0a
InteractiveNinja <gabrielnadolny2@gmail.com>
2021-09-17 08:57:13 +0200

    project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 
+//Containers
+import { StatsComponent } from './container/stats/stats.component';
+
+//Component
+import { StatsCardComponent } from './component/stats-card/stats-card.component';
+import { HttpClientModule } from '@angular/common/http';
+
+//Services
+import { StatsGithubService } from './service/stats-github.service';
+
+//Store
+
+import * as fromStore from './store';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, StatsComponent, StatsCardComponent],
   imports: [
+    CommonModule,
     BrowserModule,
-    StoreModule.forRoot({}, {})
+    HttpClientModule,
+    FormsModule,
+    StoreModule.forRoot({}, {}),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [StatsGithubService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
